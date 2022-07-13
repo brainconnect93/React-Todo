c6af40ff407dccc98eda40bca53fdf4052b91073
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-13 21:42:26 +0100

    Use the destructuring method

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -1,15 +1,30 @@
 import React from "react";
+import styling from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
     render() {
+        const completedStyle = {
+          fontStyle: "italic",
+          color: "red",
+          opacity: 0.4,
+          textDecoration: "line-through",
+        }
+
+    const { completed, id, title } = this.props.todo 
         return (
-            <li>
+            <li className={styling.item}>
                 <input 
-                type="checkbox" 
-                checked={this.props.todo.completed}
-                onChange={() => console.log("clicked")}
+                type="checkbox"
+                className={styling.checkbox}
+                checked={completed}
+                onChange={() => this.props.handleChangeProps(id)}
                 /> 
-                {this.props.todo.title}
+                <button onClick={() => this.props.deleteTodoProps(id)}>
+                    Delete
+                </button>
+                <span style={completed ? completedStyle : null}>
+                {title}
+                </span>
             </li>
         )
     }

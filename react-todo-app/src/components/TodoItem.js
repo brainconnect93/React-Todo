7da238871e89dc3849186801a492c01d9efbc438
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-13 23:08:16 +0100

    Add some styling to the todo list

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -2,6 +2,14 @@ import React from "react";
 import styling from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
+    state = {
+      editing: false,
+    }
+    handleEditing = () => {
+      this.setState({
+        editing: true,
+      })
+    }
     render() {
         const completedStyle = {
           fontStyle: "italic",
@@ -9,10 +17,20 @@ class TodoItem extends React.Component {
           opacity: 0.4,
           textDecoration: "line-through",
         }
+        
+        let viewMode = {}
+        let editMode = {}
+
+        if (this.state.editing) {
+          viewMode.display = "none"
+        } else {
+          editMode.display = "none"
+        }
 
     const { completed, id, title } = this.props.todo 
         return (
             <li className={styling.item}>
+                <div onDoubleClick={this.handleEditing} style={viewMode}>
                 <input 
                 type="checkbox"
                 className={styling.checkbox}
@@ -25,6 +43,14 @@ class TodoItem extends React.Component {
                 <span style={completed ? completedStyle : null}>
                 {title}
                 </span>
+                </div>
+                
+                <input 
+                type="text" 
+                style={editMode} 
+                className={styling.textInput}
+                value={title}
+                />
             </li>
         )
     }

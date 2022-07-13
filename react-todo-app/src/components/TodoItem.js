685582b283924672842d763a75db11a8369c3337
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-13 02:21:44 +0100

    Using the class and function component

---
+++ react-todo-app/src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <li>
+                <input 
+                type="checkbox" 
+                checked={this.props.todo.completed}
+                onChange={() => console.log("clicked")}
+                /> 
+                {this.props.todo.title}
+            </li>
+        )
+    }
+}
+
+export default TodoItem;
